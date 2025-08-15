78629b6fde84a42013c234f4a43888c2a2e8c215
chobi-1998 <shinki1025@gmail.com>
2025-08-15 19:04:42 +0900

    Add Terraform resource group tf-demo-rg

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "location" {
+  default = "japaneast"
+}
+variable "rg_name" {
+  default = "tf-demo-rg"
+}
