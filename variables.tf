113d0675570a4350ee5583be17003a7980791e07
chobi-1998 <shinki1025@gmail.com>
2025-12-15 19:07:16 +0900

    bootstrap:add strage variables

--- variables.tf
+++ variables.tf
@@ -8,3 +8,14 @@ variable "location" {
 variable "rg_name" {
   default = "tf-demo-rg"
 }
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage account name (must be globally unique, lowercase, 3-24 chars)"
+}
+
+variable "storage_container_name" {
+  type        = string
+  description = "Blob container name"
+  default     = "tfstate"
+}
