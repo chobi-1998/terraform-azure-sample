161bff05919cd4856d1e87436742844c35d414cc
新喜 淳_29978 <atsushi.shinki.61@shiftinc.jp>
2025-12-17 14:20:42 +0900

    backend change

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
+    resource_group_name  = "tf-demo-rg"
     storage_account_name = "tfstatebackend61"
     container_name       = "tfstate"
     key                  = "main.tfstate"
