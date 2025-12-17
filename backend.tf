781b4711370bab873750cbcdb950b3520ea3a4e2
新喜 淳_29978 <atsushi.shinki.61@shiftinc.jp>
2025-12-17 14:01:26 +0900

    Setup azurerm backend and local auth script template

--- backend.tf
+++ backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatebackend61"
+    container_name       = "tfstate"
+    key                  = "main.tfstate"
+  }
 }
